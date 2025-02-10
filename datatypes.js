fe35ea9d354df6d05b262abd0d798453f45f3934
Learner-Abhay <itsabhaysharma12@gmail.com>
2025-02-10 20:55:34 +0530

    Datatypes basic in JS
    This is just javascript datatypes basic.

---
+++ datatypes.js
@@ -0,0 +1,14 @@
+//Common eg of datatypes mostly primitive
+var name = "Joe";
+let score = 102;
+let isLoggedin = false;
+
+//non-primitive datatypes array and object
+let coffee = ["black coffee", "cappuchino", "decaf"];
+let user = { firstName: "Joe", lastName: "Rogan" };
+
+console.log(isLoggedin);
+console.log(score);
+console.log(name);
+console.log(coffee[0]);
+console.log(user.firstName);
