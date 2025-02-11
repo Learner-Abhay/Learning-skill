66add327a88d4f9212dd1a64ed29d1d2fd721493
Learner-Abhay <itsabhaysharma12@gmail.com>
2025-02-11 23:42:27 +0530

    Basic arithmetic operations in javascript.
    Its just for revision purpose and also for learning with basics..

---
+++ operations.js
@@ -0,0 +1,34 @@
+//Following are examples of operation on numbers..
+let addition = 5 + 10;
+let subtract = 10 - 6;
+let multi = 10 * 5;
+let divide = 10 / 5;
+let rem = 15 % 2;
+let expo = 2 **6;
+
+console.log(addition);
+console.log(subtract);
+console.log(multi);
+console.log(divide);
+console.log(rem);
+console.log(expo);
+
+//Increment operations.
+let incr = 132;
+rem++;
+console.log(incr);
+
+//decrement operations.
+let dec = 323;
+dec--;
+console.log(dec);
+
+//Comparison Operations to check values...
+let a = 10;
+let b = 20;
+
+if (a > b) {
+  console.log("a is greater than b");
+} else {
+  console.log("b is greater than a");
+}
