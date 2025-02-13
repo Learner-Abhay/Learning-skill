bc75a929a6d1b7a49ec73b40d2366794ffbbdd1a
Learner-Abhay <itsabhaysharma12@gmail.com>
2025-02-13 22:09:11 +0530

    Demonstration of Non primitives datatypes in JS
    It includes demo of array and objects please share and learn.

---
+++ nonprimitives.js
@@ -0,0 +1,15 @@
+let username = {
+  firstname: "Joe",
+  Age: "24",
+};
+
+console.log(username.Age);
+console.log(username.firstname);
+console.log(typeof username);
+
+//Arrays 
+
+let marvels=["Thor","Iron Man","Spider-Man"];
+console.log(marvels[0]);
+console.log(marvels[1]);
+console.log(marvels[2]);
