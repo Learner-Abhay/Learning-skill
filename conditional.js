2be85dc784259fc0950e6aa7a79a9eedce4fa89c
Learner-Abhay <itsabhaysharma12@gmail.com>
2025-02-14 22:43:20 +0530

    Its all practice

---
+++ conditional.js
@@ -0,0 +1,37 @@
+// let num1 = 5;
+// let num2 = 6;
+// if (num1 > num2) {
+//   console.log("num1 is greater than num2");
+// } else {
+//   console.log("num2 is greater than num1");
+// }
+
+// //Checking string
+
+// let userName="Chai";
+// let userName2="Tea";
+// if (userName == userName2) {
+//     console.log("Both are same");
+// }
+// else{
+//     console.log("Both are not same");
+// }
+
+// //Checking if types are same
+
+// let authName1 = "Chai";
+// let authName2 = "chai";
+// if (authName1 === authName2) {
+//   console.log("Both are same");
+// } else {
+//   console.log("Both are not same");
+// }
+
+//Check if variable is number or not
+
+let number = 23;
+if (typeof number === "number") {
+  console.log("it is a number");
+} else {
+  console.log("This is not a number");
+}
