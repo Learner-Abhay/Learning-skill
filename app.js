6c99e535e8b7661c9e71bdcab8b90a4717b426af
Learner-Abhay <itsabhaysharma12@gmail.com>
2025-03-05 21:00:10 +0530

    ES6 and common js
    learned import and export

---
+++ app.js
@@ -0,0 +1,11 @@
+//Module import file
+
+//default import
+import multiply from "./mathopration.js";
+
+//named import
+import { add, sub } from "./mathopration.js";
+
+console.log(multiply(2, 2));
+console.log(add(2, 2));
+console.log(sub(4, 2));
---
+++ app2.js
@@ -0,0 +1,4 @@
+const mathoperationC = require("./mathoperationC");
+
+console.log(mathoperationC.add(2, 2));
+console.log(mathoperationC.sub(5, 2));
