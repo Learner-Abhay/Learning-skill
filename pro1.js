0faeff885a939e892b1a74da969a01164f3b92da
Learner-Abhay <itsabhaysharma12@gmail.com>
2025-03-22 21:01:31 +0530

    vowels problem solved

--- pro1.js
+++ pro1.js
@@ -1,8 +1,25 @@
-let arr1 = [1, 2, 3, 4];
-let arr2 = [5, 6, 7, 8];
-let arr3 = [];
 
-for (let i = 0; i < arr1.length; i++) {
-  arr3.push(arr1[i] + arr2[i]); // ✅ Element-wise sum
+function vowels(str) {
+  let count = 0;
+  let newStr = "";
+  let vowels = "";
+  for (let i = 0; i < str.length; i++) {
+    if (
+      str[i] == "a" ||
+      str[i] == "e" ||
+      str[i] == "i" ||
+      str[i] == "o" ||
+      str[i] == "u"
+    ) {
+      count++;
+      vowels = vowels + str[i];
+    } else {
+      newStr += str[i];
+    }
+  }
+  console.log(count);
+  console.log(newStr);
+  console.log(vowels);
 }
-console.log(arr3);
+
+vowels("hello");
