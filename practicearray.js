aa1ec7e1c4ad90350d930b8a91c026d2efa58990
Learner-Abhay <itsabhaysharma12@gmail.com>
2025-03-23 23:26:28 +0530

    all problems with more efficiency

--- practicearray.js
+++ practicearray.js
@@ -1,23 +1,3 @@
-let arr1 = [
-  9, 9, 9, 9, 9, 99, 9, 9, 9, 9, 88888, 99, 9, 9, 9, 9, 9, 91, 2, 3, 4, 5, 0, 6,
-  9, 6, 7, 7, 8,
-];
-let newArr = [];
-for (let i = 0; i < arr.length; i++) {
-  if (arr1[i] < 101) {
-    newArr.push(arr1[i]);
-  }
-}
-console.log(newArr);
-
-let arr = [45, 56, 78, 34, 99];
-let sum = 0;
-for (let val of arr) {
-  sum += val;
-}
-let avg = sum / arr.length;
-console.log(avg);
-
 let itemsp = [250, 645, 300, 900, 50];
 let finalPrice = [];
 
@@ -27,3 +7,11 @@ for (let val of itemsp) {
   finalPrice.push(val);
 }
 console.log(finalPrice);
+
+let arr = [23, 45, 45, 46, 78];
+let sum = 0;
+
+for (let val of arr) {
+  sum = sum + val;
+}
+console.log(sum);
